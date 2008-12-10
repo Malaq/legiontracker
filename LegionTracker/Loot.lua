36f806bc85fa1429f01d678e507f2cce20ff95fa
darin.tay <darin.tay@c89229fa-c5c7-11dd-ac03-07ed41fb8567>
2008-12-10 06:32:03 +0000

    Parsing out item ids from loot and starting up some tables.

--- LegionTracker/Loot.lua
+++ LegionTracker/Loot.lua
@@ -1,4 +1,9 @@
-LootFrame = CreateFrame("Frame", "LegionTrackerLoot");
+lootFrame = CreateFrame("Frame", "LegionTrackerLoot");
+-- playername -> { loot_ids }
+playerLootTable = {};
+-- loot_id -> { ... }
+lootIdTable = {};
+
 
 function Print(text)
 	DEFAULT_CHAT_FRAME:AddMessage(text, 1, 1, 0);
@@ -6,10 +11,27 @@ end
 
 function Loot_OnEvent(this, event, arg1)
 	if event == "CHAT_MSG_LOOT" then
-		Print(string.format("%s", string.sub(arg1, string.find(arg1, "|c.*|r"))));
+		local player = "Happyduude";
+		local name, link, rarity = GetItemInfo(string.sub(arg1, string.find(arg1, "|c.*|r")));
+		local _, color, itemString = strsplit("|", link);
+		Print(string.format("Item string: %s", itemString));
+		Print(string.format("Item link: %s", link));
+		local _, itemId = strsplit(":", itemString);
+		Print(string.format("Item id: %d  item name: %s", itemId, name));
+
+
+		if (playerLootTable[player] == nil) then
+			playerLootTable[player] = {};
+		end
+
+		playerLootTable[player][itemId] = 1;
+
+		for key, value in pairs(playerLootTable[player]) do
+			Print(string.format("key: %d  value: %d", key, value));
+		end
 	end
 end
 
-LootFrame:SetScript("OnEvent", Loot_OnEvent);
-LootFrame:RegisterEvent("CHAT_MSG_LOOT");
+lootFrame:SetScript("OnEvent", Loot_OnEvent);
+lootFrame:RegisterEvent("CHAT_MSG_LOOT");
 
