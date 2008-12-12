0c0bd4b6026ad8004cee7282703f4b287eec5b95
mikalc <mikalc@c89229fa-c5c7-11dd-ac03-07ed41fb8567>
2008-12-12 04:39:58 +0000

    Buggy

--- trunk/LegionTracker/LT_Timer.lua
+++ trunk/LegionTracker/LT_Timer.lua
@@ -2,14 +2,19 @@
 TIMER_START = time();
 TIMER_TOTAL = 0;
 LT_Timer_UpdateInterval = 1.0;
-LT_Timer_AttendanceInterval = 10;
 
 function LT_TimerOnLoad()
     LT_Print("Loaded LT_Timer");
+    if (LT_TimerInterval == nil) then
+        LT_TimerInterval = {};
+        LT_SetInterval(1,"min");
+        LT_SetInterval(0,"sec");
+        LT_Print("LT_Timer: min-"..LT_TimerInterval["min"].." sec-"..LT_TimerInterval["sec"].." total-"..LT_TimerInterval["total"]);
+    end
 end
 
 function LT_Timer_Clicked(args)
-    LT_Print("Clicked: "..args);
+    --LT_Print("Clicked: "..args);
     if (args == "LeftButton") then
         LT_TimerToggle();
     elseif (args == "RightButton") then
@@ -18,18 +23,52 @@ function LT_Timer_Clicked(args)
 end
 
 function LT_Timer_Settings_OnShow()
-    form_label = getglobal("LT_Timer_Settings".."TimeInterval");
-    form_label:SetText(LT_GetInterval());
+    min_int = getglobal("LT_Timer_Settings".."MinInterval");
+    min_int:SetText(""..LT_GetInterval("min"));
+    min_label = getglobal("LT_Timer_Settings".."MinLabel".."Label");
+    min_label:SetText("Min:");
+    
+    sec_int = getglobal("LT_Timer_Settings".."SecInterval");
+    sec_int:SetText(""..LT_GetInterval("sec"));
+    sec_label = getglobal("LT_Timer_Settings".."SecLabel".."Label");
+    sec_label:SetText("Sec:");
 end
 
-function LT_SetInterval(args)
-    if (args ~= nil) then
-        LT_Timer_AttendanceInterval = args;
+function LT_SetInterval(args, arg2)
+    if (arg2 ~= nil) then
+        if (arg2 == "sec") then
+            LT_TimerInterval["sec"] = args;
+            if (LT_TimerInterval["min"] == nil) then
+                LT_TimerInterval["min"] = 0;
+            end
+        elseif (arg2 == "min") then
+            LT_TimerInterval["min"] = args;
+            if (LT_TimerInterval["sec"] == nil) then
+                LT_TimerInterval["sec"] = 0;
+            end
+        end
+        LT_TimerInterval["total"] = LT_TimerInterval["min"]*60 + LT_TimerInterval["sec"];
+    else
+        LT_TimerInterval["total"] = args;
     end
+    LT_Print("Timer reset to total: "..LT_TimerInterval["total"]);
 end
 
-function LT_GetInterval()
-    return LT_Timer_AttendanceInterval;
+function LT_GetInterval(args)
+    if (LT_TimerInterval == nil) then
+        LT_Print("DEBUG:  TABLE IS EMPTY");
+        return "Err.";
+    end
+    if (args == nil) then
+        LT_Print("DEBUG1: "..LT_TimerInterval["total"]);
+        return ""..LT_TimerInterval["total"];
+    elseif (args == "min") then
+        LT_Print("DEBUG2: "..LT_TimerInterval["min"]);
+        return ""..LT_TimerInterval["min"];
+    elseif (args == "sec") then
+        LT_Print("DEBUG3: "..LT_TimerInterval["sec"]);
+        return ""..LT_TimerInterval["sec"];
+    end
 end
 
 function LT_TimerToggle()
@@ -42,7 +81,7 @@ function LT_TimerToggle()
         timer_label = getglobal("LT_Main".."Timer".."Label");
         timer_label:SetText("Starting timer...");
         TIMER_TOGGLE = "true";
-    end
+    end 
 end
 
 function LT_TimerOnUpdate(self, elapsed)
@@ -53,7 +92,7 @@ function LT_TimerOnUpdate(self, elapsed)
         if (self.TimeSinceLastUpdate > LT_Timer_UpdateInterval) then
             --DEFAULT_CHAT_FRAME:AddMessage("Thrashing?");
             TIMER_TOTAL = difftime(time(), TIMER_START);
-            TICKER = mod(TIMER_TOTAL, LT_Timer_AttendanceInterval);
+            TICKER = mod(TIMER_TOTAL, LT_TimerInterval["total"]);
             --Prevent painting data if the window its in is not shown.
             if (LT_Main:IsShown()) then
                 TIMER_SEC = mod(TIMER_TOTAL,60);
@@ -69,7 +108,7 @@ function LT_TimerOnUpdate(self, elapsed)
             
             --If your specified interval has been met, run the code (watch out for lag problems here)
             if (TICKER == 0) and (TIMER_TOTAL ~= 0) then
-                LT_Print(LT_Timer_AttendanceInterval.." SECONDS PASSED!!!","bleh");
+                LT_Print(LT_TimerInterval["total"].." SECONDS PASSED!!!","bleh");
                 --Add attendance ticking code here!
             end
         end
