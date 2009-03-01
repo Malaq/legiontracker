ea010ecbdc0a4aa1eb70ee11f18e925d1d4af9f8
mikalc <mikalc@c89229fa-c5c7-11dd-ac03-07ed41fb8567>
2009-03-01 00:18:55 +0000

    Modified /lt version

--- trunk/LegionTracker/LT_Settings.lua
+++ trunk/LegionTracker/LT_Settings.lua
@@ -1,7 +1,9 @@
 ﻿function LT_Settings_VersionCheck()
-    SendChatMessage("Running version check...", "OFFICER");
-    local cmd = {type = "VersionCheck"};
+    --SendChatMessage("Running version check...", "OFFICER");
+    LT_Print("LegionTracker: Running version check...", "yellow");
+    LT_Print(UnitName("player").. ": " ..LT_VERSION);
+    local cmd = {type = "VersionCheck", player = UnitName("player")};
     LT_OfficerLoot:SendOfficerMessage("LT_OfficerLoot_Command", LT_OfficerLoot:Serialize(cmd));
     cmd = {type = "VersionResponse", version = LT_VERSION, player = UnitName("player")};
-    self:SendOfficerMessage("LT_OfficerLoot_Command", LT_OfficerLoot:Serialize(cmd));
+    LT_OfficerLoot:SendOfficerMessage("LT_OfficerLoot_Command", LT_OfficerLoot:Serialize(cmd));
 end
